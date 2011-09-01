472c0ddc1b23c07738fc5f333b8c5587e22e631b
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-09-01 16:52:54 +0300

    hello

--- app/controllers/tropo_controller.rb
+++ app/controllers/tropo_controller.rb
@@ -1,12 +1,10 @@
 class TropoController < ApplicationController
   def index
-    response = Tropo::Generator.say 'Hello World!'
-    puts "Tropo Response : #{response}"
-    render :text => response, :content_type => 'application/json'
+    render :text => 'hello'
   end
 
   def hello 
-    response = Tropo::Generator.say 'Hello World!'
+    response = Tropo::Generator.say 'Hello World! Would you like a piece of cake?'
     puts "Tropo Response : #{response}"
     render :text => response, :content_type => 'application/json'
   end
