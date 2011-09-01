fbafc58b7b708f01477e9825e247aa8fc7d886b0
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-09-01 14:59:54 +0300

    herbox app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herbox::Application.config.secret_token = '9e869ddf9e8246182460605b6b025f2874022f6bb67ccdd497fc250b63aaffb72e0466104c580081449216ec75987ba83eb78d953c80854e4c83a041435b9ebe'
