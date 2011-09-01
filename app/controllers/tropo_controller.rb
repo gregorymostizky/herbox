edc314fe0f1d3d83922f16fdad3434ec36d6f50d
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-09-01 18:50:57 +0300

    sending IM

--- app/controllers/tropo_controller.rb
+++ app/controllers/tropo_controller.rb
@@ -1,12 +1,23 @@
+require 'open-uri'
+
 class TropoController < ApplicationController
   def index
     render :text => 'hello'
   end
 
   def hello 
-    response = Tropo::Generator.say 'Hello World! Would you like a piece of cake?'
-    puts "Tropo Response : #{response}"
-    render :text => response, :content_type => 'application/json'
+#    response = Tropo::Generator.say 'Hello World! Would you like a piece of cake?'
+#    puts "Tropo Response : #{response}"
+
+    tropo = Tropo::Generator.new do
+      call(:to => 'gregory.mostizky@gmail.com', :network => 'Jabber')
+      say(:value => 'Game Over Man')
+    end
+    
+    render :text => tropo.response, :content_type => 'application/json'
   end
 
+  def start 
+    open("https://api.tropo.com/1.0/sessions?action=create&token=#{TROPO_TOKEN_MESSAGING}")
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Herbox::Application.initialize!
+
+TROPO_TOKEN_MESSAGING = '076abc512574ff4aab338d99e5d48ddeec5d23fefbf0b7a24a964ab8085eb306e681079ec8dc72a5aa46140c'
+TROPO_TOKEN_VOICE = '076aba8875e2024082a4cb50ff9839858055b81c9f48a4b9eb4e08991c313435fe55af292fa5ab152ca2efdc'
