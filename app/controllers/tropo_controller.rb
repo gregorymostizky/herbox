63afc569796a49d47e9770901c8a2566dadf7c56
Gregory Mostizky <gregory.mostizky@gmail.com>
2011-09-01 20:19:41 +0300

    conversation params

--- app/controllers/tropo_controller.rb
+++ app/controllers/tropo_controller.rb
@@ -5,18 +5,22 @@ class TropoController < ApplicationController
   end
 
   def hello 
+    to = params[:session][:params][:name]
+    msg = params[:session][:params][:msg]
+
 #    response = Tropo::Generator.say 'Hello World! Would you like a piece of cake?'
 #    puts "Tropo Response : #{response}"
 
     tropo = Tropo::Generator.new do
-      call(:to => 'gregory.mostizky@gmail.com', :network => 'Jabber')
-      say(:value => 'Game Over Man')
+      call(:to => to, :network => 'Jabber')
+      say(:value => msg)
     end
     
     render :text => tropo.response, :content_type => 'application/json'
   end
 
   def start 
-    open("https://api.tropo.com/1.0/sessions?action=create&token=#{TROPO_TOKEN_MESSAGING}&name=#{params[:name]}&msg=#{params[:msg]}")
+    open("https://api.tropo.com/1.0/sessions?action=create&token=#{TROPO_TOKEN_MESSAGING}&name=#{params[:start][:name]}&msg=#{params[:start][:msg]}")
+    render :text => 'Conversation Started'
   end
 end
